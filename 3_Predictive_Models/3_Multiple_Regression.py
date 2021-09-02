b9a1cbbe21ecedcbc6954201f12618025be49c2c
Andy Blankley <andrew.blankley@hotmail.com>
2021-09-02 17:44:40 +0100

    Example performed in class for multiple regression

--- 3_Predictive_Models/3_Multiple_Regression.py
+++ 3_Predictive_Models/3_Multiple_Regression.py
@@ -12,6 +12,31 @@ groups = df1.groupby(pd.cut(df1['Mileage'],bins)).mean()
 print(groups.head())
 groups['Price'].plot.line()
 
+import statsmodels.api as sm
+from sklearn.preprocessing import StandardScaler
+
+scale = StandardScaler()
+
+x = df[['Mileage', 'Cylinder', 'Doors']]
+y = df['Price']
+
+x[['Mileage', 'Cylinder', 'Doors']] = scale.fit_transform(x[['Mileage', 'Cylinder', 'Doors']].values)
+
+## Add a constant column to our model so we can have a Y intercept
+print(x)
+
+est = sm.OLS(y,x).fit()
+print(est.summary())
+
+##### Lets make an actual prediction for the data.
+# checking if more doors means higher price
+y.groupby(df.Doors).mean()
+
+scaled = scale.transform([[45000,8,4]])
+scaled = np.insert(scaled[0,0,1])  ## scaled array in format necessary, position 0, value of 1
+print(scaled)
+predicted = est.predict(scaled)
+print(predicted)
 
 
 

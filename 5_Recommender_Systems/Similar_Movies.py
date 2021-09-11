fc9695e39ebdc552c73b37a4487bf50a062d662c
Andy Blankley <andrew.blankley@hotmail.com>
2021-09-11 20:04:26 +0100

    Enhancement of Similar Movies
    Enhancement of user based recommendations done. Grouped the movies ratings by title and compute number of ratings and the average rating. Pull out all the movies that have more than N number of ratings.
    Sort these new results and create a new DF joining previous recommendation table to show similiarity.

--- 5_Recommender_Systems/Similar_Movies.py
+++ 5_Recommender_Systems/Similar_Movies.py
@@ -12,19 +12,21 @@ movies = pd.read_csv('../0_data/ml/u.item', sep='|', names=m_cols, usecols=range
 
 ratings = pd.merge(movies, ratings)
 
-print(ratings.head())
+print("Ratings:\n", ratings.head())
+print()
 
 # Now the amazing pivot_table function on a DataFrame will construct a user / movie rating matrix.
 # Note how NaN indicates missing data - movies that specific users didn't rate.
 
 movieRatings = ratings.pivot_table(index=['user_id'],columns=['title'],values='rating')
-print(movieRatings.head())
+print("Moving Ratings:\n", movieRatings.head())
 
 print()
 
 # Let's extract a Series of users who rated Star Wars:
 starWarsRatings = movieRatings['Star Wars (1977)']
-print(starWarsRatings.head())
+print("Star Wars Ratings:\n", starWarsRatings.head())
+print()
 
 # Pandas' corrwith function to compute the pairwise correlation of Star Wars' vector of user rating
 # with every other movie. After drop any results that have no data,
@@ -33,11 +35,34 @@ print(starWarsRatings.head())
 similarMovies = movieRatings.corrwith(starWarsRatings) # Create a vector with correlation of all movies.
 similarMovies = similarMovies.dropna() # drop the NA values from the set.
 df = pd.DataFrame(similarMovies) # create a dataframe from this data.
-print(df.head(10)) # View the first 10 rows.
+print("Similar Movie Preview (before sort)\n", df.head(10)) # View the first 10 rows.
+print()
 
 ## Sort results:
-print(similarMovies.sort_values(ascending=False))
+print("Similar Movies Results:\n", similarMovies.sort_values(ascending=False))
+print()
 
 ## Results do not show anything of value. Movies shown may correlate, but have no relationship with star wars as a genre.
+print("----------------------------------")
+
+# we need to get rid of movies that were only watched by a few people that are producing spurious results
+# Let's construct a new DataFrame that counts up how many ratings exist for each movie, and also the average
+# rating while we're at it.
+
+import numpy as np
+movieStats = ratings.groupby('title').agg({'rating': [np.size, np.mean]})
+print("Movie Stats:\n", movieStats.head())
+print()
+
+# Let's get rid of any movies rated by fewer than X people,
+# and check the top-rated ones that are left:
+popularMovies = movieStats['rating']['size'] >= 100
+movieStats[popularMovies].sort_values([('rating', 'mean')], ascending=False)[:15]
+
+# Let's join this data with our original set of similar movies to Star Wars:
+df = movieStats[popularMovies].join(pd.DataFrame(similarMovies, columns=['similarity']))
+# sort these new results by similarity score.
+df = df.sort_values(['similarity'], ascending=False)[:15]
+print("Final Recommendation:\n", df.head())
 
 

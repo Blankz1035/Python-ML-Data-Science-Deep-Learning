07a2eba2ab281784b00900ed3f0508ec39cd0c04
Andy Blankley <andrew.blankley@hotmail.com>
2021-09-13 21:39:34 +0100

    Create Principle Component Analysis
    Initial commit and introduction.

---
+++ 6_More_ML_DM_Techniques/Principle_Component_Analysis.py
@@ -0,0 +1,6 @@
+# PCA is a dimensionality reduction technique:
+# it lets you distill multi-dimensional data down to fewer dimensions,
+# selecting new dimensions that preserve variance in the data as best it can
+
+# Example using IRIS dataset. This DS has a total of 4 dimensions.
+# We will do PCA on this to transform the DS into a lower dimension ratio and review the results.

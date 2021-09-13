69ab15fa46daadc6b85eb5c2e4add7f2a5314ff1
Andy Blankley <andrew.blankley@hotmail.com>
2021-09-13 21:52:06 +0100

    Explained Variance Review
    Review the variance of the PCA model. 3 dimensions giving  approx 0.99, and 2 giving 0.97. With this DS, in 1 dimension we can preserve 0.92 % of the variance. This shows we can  preserve a huge amount of the data while compressing.

--- 6_More_ML_DM_Techniques/Principle_Component_Analysis.py
+++ 6_More_ML_DM_Techniques/Principle_Component_Analysis.py
@@ -21,18 +21,28 @@ print(list(iris.target_names))
 print()
 
 
+X = iris.data
+pca = PCA(n_components=2, whiten=True).fit(X) # n_component will represent the dimensions we want to tranform.
+# Whiten will normalize all the data
+X_pca = pca.transform(X)
+
+print("PCA Components: \n", pca.components_)
+print()
+
+print("Explained Variance:")
+print(pca.explained_variance_ratio_)
+print(sum(pca.explained_variance_ratio_))
+print()
+# PCA will optimize and choose the best dimensionality to preserve the data.
 
 
-
-
-
-
-
-
-
-
-
-
-
-
+from pylab import *
+colors = cycle('rgb')
+target_ids = range(len(iris.target_names))
+pl.figure()
+for i, c, label in zip(target_ids, colors, iris.target_names):
+    pl.scatter(X_pca[iris.target == i, 0], X_pca[iris.target == i, 1],
+        c=c, label=label)
+pl.legend()
+pl.show()
 

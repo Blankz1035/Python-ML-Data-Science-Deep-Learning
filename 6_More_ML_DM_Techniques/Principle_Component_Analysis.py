7ffcb74b4580eadb9718018efe62874a6d5d811c
Andy Blankley <andrew.blankley@hotmail.com>
2021-09-13 21:42:46 +0100

    Data Inspection
    Inspect the ds and determine dimensionality

--- 6_More_ML_DM_Techniques/Principle_Component_Analysis.py
+++ 6_More_ML_DM_Techniques/Principle_Component_Analysis.py
@@ -4,3 +4,35 @@
 
 # Example using IRIS dataset. This DS has a total of 4 dimensions.
 # We will do PCA on this to transform the DS into a lower dimension ratio and review the results.
+
+from sklearn.datasets import load_iris
+from sklearn.decomposition import PCA
+import pylab as pl
+from itertools import cycle
+
+## Load the DS using import
+iris = load_iris()
+
+## shape returns a tuple of the size of the dataset and the number of features -> dimensions.
+numSamples, numFeatures = iris.data.shape
+print(numSamples)
+print(numFeatures)
+print(list(iris.target_names))
+print()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
